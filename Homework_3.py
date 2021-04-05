b5027f5f60a3f2cc2f9f291367409a6a087210cc
Oleh Lavrynovych <oleh.lavrynovych@eleks.com>
2021-04-05 13:40:38 +0300

    Homework 3

---
+++ Homework_3.py
@@ -0,0 +1,20 @@
+def calculate_fibonacci(number):
+    if __name__ = '__main__'
+        if number == 1:
+            return 1
+        if number == 0:
+            return 0
+        else:
+            return calculate_fibonacci(number-1) + calculate_fibonacci(number-2)
+
+
+
+
+
+##
+##        def fibonacci(n):
+##    if n in (1, 2):
+##        return 1
+##    return fibonacci(n - 1) + fibonacci(n - 2)
+##      
+    
