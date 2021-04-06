8f463736036fd36ef2055491b4dcf560a70f4a20
Oleh Lavrynovych <oleh.lavrynovych@eleks.com>
2021-04-06 11:13:04 +0300

    add main function

--- Homework_3.py
+++ Homework_3.py
@@ -1,20 +1,32 @@
 def calculate_fibonacci(number):
-    if __name__ = '__main__'
-        if number == 1:
-            return 1
-        if number == 0:
-            return 0
-        else:
-            return calculate_fibonacci(number-1) + calculate_fibonacci(number-2)
+    
+    if number == 0:
+        return 0
+    if number in (1, 2):
+        return 1
+    return calculate_fibonacci(number - 1) + calculate_fibonacci(number - 2)
+
+
+def main():
+    
+    n = 0
+    n = int(input('Enter number:'))
+    result = calculate_fibonacci(n)
+    print(f'Fibonacci result is: {result}')
+
+    if __name__ == '__main__':
+        main()
 
 
 
 
 
-##
-##        def fibonacci(n):
-##    if n in (1, 2):
+##    if number == 1:
 ##        return 1
-##    return fibonacci(n - 1) + fibonacci(n - 2)
-##      
+##    if number == 0:
+##        return 0
+##    else:
+##         return calculate_fibonacci(number-1) + calculate_fibonacci(number-2)
+
+
     
